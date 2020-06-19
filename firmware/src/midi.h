fecd2961521e757bd311f3cb3db8f77ffe1aeae8
julbouln <jboulnois@gmail.com>
2020-06-19 19:10:10 +0200

    add missing file

---
+++ firmware/src/midi.h
@@ -0,0 +1,12 @@
+#ifndef __MIDI_H
+#define __MIDI_H
+
+#define MIDI_NOTE_OFF 0x80
+#define MIDI_NOTE_ON 0x90
+#define MIDI_POLY_PRESSURE 0xA0
+#define MIDI_CONTROL_CHANGE 0xB0
+#define MIDI_PROGRAM_CHANGE 0xC0
+#define MIDI_CHANNEL_PRESSURE 0xD0
+#define MIDI_PITCH_BEND 0xE0
+
+#endif 
